6bf588b98e751ba32fe54ba4d66fd7529a127cab
Jesse50gaming <128247303+Jesse50gaming@users.noreply.github.com>
2026-01-01 15:07:12 -0600

    progress

--- src/client/java/com/macromod/macros/MiningMacro.java
+++ src/client/java/com/macromod/macros/MiningMacro.java
@@ -1,8 +1,13 @@
 package com.macromod.macros;
 
-import com.jcraft.jorbis.Block;
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
+
 import com.macromod.Utils;
 
+import net.minecraft.block.Block;
+import net.minecraft.block.Blocks;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.entity.MovementType;
 import net.minecraft.util.Hand;
@@ -23,13 +28,15 @@ public class MiningMacro {
     private static int moveCounter = 0;
     private static final int ticksOfForwardMovement =5; 
 
+    static ArrayList<BlockPos> detectedOres = new ArrayList<>();
+
     public enum MiningMacroStates {
         IDLE,
         MINING_TOP_BLOCK,
         MINING_BOTTOM_BLOCK,
-        SCAN_ORES,
         MINE_ORES,
-        MOVE_FOWARD
+        MOVE_FOWARD,
+        SCAN_ORES
     }
 
     public static void start(MinecraftClient c) {
@@ -76,14 +83,14 @@ public class MiningMacro {
             case MINING_BOTTOM_BLOCK:
                 updateMineBottomBlock();
                 break;
-            case SCAN_ORES:
-                // Logic to scan for ores
-                break;
             case MINE_ORES:
-                // Logic to mine detected ores
+                updateMineOres();
                 break;
             case MOVE_FOWARD:
                 updateMoveForward();
+                break;
+            case SCAN_ORES:
+                // No continuous update needed
                 break;
         }
 
@@ -102,15 +109,15 @@ public class MiningMacro {
            case MINING_BOTTOM_BLOCK:
                initMineBottomBlock();
                break;
-           case SCAN_ORES:
-               // Logic to initialize scanning for ores
-               break;
            case MINE_ORES:
-               // Logic to initialize mining detected ores
+               initMineOres();
                break;
            case MOVE_FOWARD:
                initMoveForward();
                break;
+            case SCAN_ORES:
+                initScanOres();
+                break;
        }
 
        currentState = newState;
@@ -147,7 +154,7 @@ public class MiningMacro {
     
 
     private static void startMiningBlock() {
-
+        updateMiningSide(targetBlockPos);
         client.interactionManager.attackBlock(targetBlockPos, targetBlockSide);
         client.player.swingHand(Hand.MAIN_HAND);
     }
@@ -168,7 +175,7 @@ public class MiningMacro {
         
         if (client.world.isAir(targetBlockPos)) {
             
-            updateState(MiningMacroStates.MOVE_FOWARD);
+            updateState(MiningMacroStates.SCAN_ORES);
             
         }
     }
@@ -186,6 +193,87 @@ public class MiningMacro {
             client.options.forwardKey.setPressed(false);
             updateState(MiningMacroStates.MINING_TOP_BLOCK);
         }
+    }
+
+    private static void initScanOres() {
+        detectedOres = detectOres();
+    }
+
+    private static void initMineOres() {
+        
+        setTargetBlock(detectedOres.get(0));
+        startMiningBlock();
+    }
+
+    private static ArrayList<BlockPos> detectOres() {
+        ArrayList<BlockPos> orePositions = new ArrayList<>();
+
+        BlockPos playerPos = client.player.getBlockPos();
+
+        
+        //central -- to make sure to get above and below
+        for (int z = -1; z <=2; z++) {
+            BlockPos pos = playerPos.add(0, 0, z);
+            Block block = client.world.getBlockState(pos).getBlock();
+            if (isOre(block)) {
+                orePositions.add(pos);
+            }
+        }
+        // cross -> +
+        for (int z = 0; z <=2; z++) {
+            for (int x = -1; x <=1; x++) {
+                BlockPos pos = playerPos.add(x, 0, z);
+                Block block = client.world.getBlockState(pos).getBlock();
+                if (isOre(block)) {
+                    orePositions.add(pos);
+                }
+            }
+            for (int y = -1; y <=1; y++) {
+                BlockPos pos = playerPos.add(0, y, z);
+                Block block = client.world.getBlockState(pos).getBlock();
+                if (isOre(block)) {
+                    orePositions.add(pos);
+                }
+            }
+            
+        }
+
+ 
+        return orePositions;
+    }
+
+    private static boolean isOre(Block block) {
+        return block == Blocks.COAL_ORE || block == Blocks.DEEPSLATE_COAL_ORE || block == Blocks.IRON_ORE || block == Blocks.DEEPSLATE_IRON_ORE ||  
+        block == Blocks.GOLD_ORE || block == Blocks.DEEPSLATE_GOLD_ORE || block == Blocks.DIAMOND_ORE || block == Blocks.DEEPSLATE_DIAMOND_ORE || 
+        block == Blocks.REDSTONE_ORE || block == Blocks.DEEPSLATE_REDSTONE_ORE || block == Blocks.LAPIS_ORE || block == Blocks.DEEPSLATE_LAPIS_ORE || 
+        block == Blocks.EMERALD_ORE || block == Blocks.DEEPSLATE_EMERALD_ORE;
+    }
+
+
+    private static void updateMineOres() {
+        client.interactionManager.updateBlockBreakingProgress(targetBlockPos, targetBlockSide);
+
+        if (client.world.isAir(targetBlockPos)) {
+            if (detectedOres.size() > 1) {
+                detectedOres.remove(0);
+                updateState(MiningMacroStates.MINE_ORES);
+            } else {
+                updateState(MiningMacroStates.MOVE_FOWARD); //TODO if it is beneath the player they will have to jump
+            }
+        } 
+    }
+
+
+    private static void updateMiningSide(BlockPos pos) {
+        Vec3d eyes = client.player.getEyePos();
+        Vec3d center = Vec3d.ofCenter(pos);
+        Direction dir = Direction.getFacing(
+            center.x - eyes.x,
+            center.y - eyes.y,
+            center.z - eyes.z
+        );
+        targetBlockSide = dir;
+        
     }
 
 
